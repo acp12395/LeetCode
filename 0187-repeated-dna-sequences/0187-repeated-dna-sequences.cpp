403546f929c8bb7929bb19820c4ae17f84e1070a
Alejandro Chávez Padilla <64603767+acp12395@users.noreply.github.com>
2024-01-17 09:24:31 -0600

    Time: 129 ms (11.96%), Space: 48.2 MB (9.59%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_multimap<int,int> hashIndexesMap;
+        int a = 7;
+        int m = 1000000007;
+        int k = 10;
+        int hash = 0;
+        vector<string> retVec;
+        if(s.size() > k)
+        {
+            for(int nucleotideIndex = 0; nucleotideIndex < k; nucleotideIndex++)
+            {
+                hash += static_cast<int>(s[nucleotideIndex]&7)*pow(a,k-(nucleotideIndex+1));
+            }
+            hash = hash % m;
+            hashIndexesMap.insert(make_pair(hash,0));
+            for(int nucleotideIndex = 1; nucleotideIndex < s.size()-(k-1); nucleotideIndex++)
+            {
+                hash = static_cast<int>((hash-(static_cast<int>(s[nucleotideIndex-1]&7)*pow(a,k-1)))*a+static_cast<int>(s[nucleotideIndex+(k-1)]&7) % m);
+                hashIndexesMap.insert(make_pair(hash,nucleotideIndex));
+            }
+            
+            {
+                auto mapIt = hashIndexesMap.begin();
+                while(mapIt != hashIndexesMap.end())
+                {
+                    auto range = hashIndexesMap.equal_range((*mapIt).first);
+                    unordered_set<string> repeatedStringsDetector;
+                    unordered_set<string> uniqueStringsSet;
+                    for(auto& index = range.first; index != range.second; index++)
+                    {
+                        string str = s.substr((*index).second,10);
+                        if(!(repeatedStringsDetector.insert(str).second))
+                        {
+                            uniqueStringsSet.insert(str);
+                        }
+                    }
+                    for(auto& str : uniqueStringsSet)
+                    {
+                        retVec.push_back(str);
+                    }
+                    mapIt = range.second;
+                }
+            }
+        }
+        return retVec;
+    }
+};
