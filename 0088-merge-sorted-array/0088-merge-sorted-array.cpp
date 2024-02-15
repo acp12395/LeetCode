b656cb1c0e83a99d1f9197fcd7a6ca6cad9b5e15
Alejandro Chávez Padilla <64603767+acp12395@users.noreply.github.com>
2024-02-15 07:56:34 -0600

    Time: 0 ms (100.00%), Space: 10.9 MB (31.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,56 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums1Cpy = nums1;
-        vector<int> nums2Cpy = nums2;
-        vector<int> result;
-        bool gt0Flag = false;
-        for(auto& i : nums1)
+        int mPtr = 0, nPtr = 0;
+        for(; nPtr < n; mPtr++)
         {
-            if(m == 0)
+            if(nums1[mPtr] > nums2[nPtr] || mPtr >= m)
             {
-                result.push_back(nums2Cpy[0]);
-                nums2Cpy.erase(nums2Cpy.begin());
-                n--;
-            }
-            else if(n == 0)
-            {
-                if(!(nums1Cpy[0] == 0 && gt0Flag == true))
+                for(int i = nums1.size() - 1; i > mPtr; i--)
                 {
-                    result.push_back(nums1Cpy[0]);
-                    nums1Cpy.erase(nums1Cpy.begin());
-                    m--;
+                    nums1[i] = nums1[i - 1];
                 }
-            }
-            else
-            {
-                if(gt0Flag && nums1Cpy[0] == 0)
-                {
-                    result.push_back(nums2Cpy[0]);
-                    nums2Cpy.erase(nums2Cpy.begin());
-                    n--;
-                }
-                else
-                {
-                    if(nums1Cpy[0] < nums2Cpy[0])
-                    {
-                        result.push_back(nums1Cpy[0]);
-                        nums1Cpy.erase(nums1Cpy.begin());
-                        m--;
-                    }
-                    else
-                    {
-                        result.push_back(nums2Cpy[0]);
-                        nums2Cpy.erase(nums2Cpy.begin());
-                        n--;
-                    }
-                    if( *(result.end() - 1) > 0)
-                    {
-                        gt0Flag = true;
-                    }
-                }
+                nums1[mPtr] = nums2[nPtr];
+                nPtr++;
+                m++;
             }
         }
-        nums1 = result;
     }
 };
